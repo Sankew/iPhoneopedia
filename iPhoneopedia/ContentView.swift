6c51534f2bd7e587d5cba33784db652e5c7ad129
Sankew <sankeshwarsivakumar@gmail.com>
2022-05-27 18:29:38 +0530

    Initial Commit

---
+++ iPhoneopedia/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iPhoneopedia
+//
+//  Created by Sankeshwar Sivakumar  on 27/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iPhoneopedia/iPhoneopediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPhoneopediaApp.swift
+//  iPhoneopedia
+//
+//  Created by Sankeshwar Sivakumar  on 27/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct iPhoneopediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
